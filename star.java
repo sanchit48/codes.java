8a597b06a8cd9d8ceb046ee1f24ab9f3c1657494
Sanchit Khare <Sanchitkhare3@gmail.com>
2019-04-28 10:48:09 +0530

    Program to write stars

---
+++ star.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+class Sum
+ {
+	public static void main (String[] args)
+{
+	 // Code
+	 for (var i = 1; i <= 5; i++) {
+		 for (var j=1;j<=i;j++) {
+			 System.out.print("* ");
+		 }	 
+		 System.out.println();
+	 }
+}
+}
+
