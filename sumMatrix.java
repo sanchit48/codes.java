3a74f469257766a0a9ac7713ccbec92831605f7c
Sanchit Khare <Sanchitkhare3@gmail.com>
2019-05-28 19:59:51 +0530

    This adds tow matrix using OOPs

---
+++ sumMatrix.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.util.Scanner;
+class Matrix
+{
+	int r,c;
+	String s;
+	int arr[][];
+	Scanner sc = new Scanner(System.in);
+	Matrix(int r, int c)
+	{
+		this.r = r;
+		this.c = c;
+		arr = new int[r][c];
+	}
+
+	int[][] getMatrix()
+	{
+		System.out.println("Enter elements for first matrix");
+		
+		for(int i=0;i<r;i++)
+		{
+			for(int j=0;j<c;j++)
+				arr[i][j] = sc.nextInt();
+		}
+		System.out.println();
+		return arr;
+		
+	}
+
+	int[][] findSum(int a[][], int b[][])
+	{
+		int[][] temp = new int[r][c];
+		for(int i=0;i<r;i++)
+		{
+			for(int j=0;j<c;j++)
+				temp[i][j] = a[i][j]+b[i][j];
+		}
+		return temp;
+	}
+
+	void displayMatrix(int temp[][])
+	{
+		System.out.println("The sum matrix is:");
+		for(int i=0;i<r;i++)
+		{
+			for(int j=0;j<c;j++)
+				System.out.print(temp[i][j]+" ");
+			
+			System.out.println();
+		}
+	}
+}
+
+class Methods
+{
+	public static void main(String args[]) throws IOException
+	{
+		Matrix obj1 = new Matrix(3,3);
+		Matrix obj2 = new Matrix(3,3);
+		int x[][] = obj1.getMatrix();
+		int y[][] = obj2.getMatrix();
+		int z[][] = obj1.findSum(x,y);
+		obj1.displayMatrix(z);
+	}
+}
