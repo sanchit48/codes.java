ab5086692ea761121748a81635d1bf1c839c1c02
Sanchit Khare <Sanchitkhare3@gmail.com>
2019-06-03 15:11:39 +0530

    This code explains recursion

---
+++ recursion.java
@@ -0,0 +1,32 @@
+class Employee implements Cloneable
+{
+	String name;
+	int id;
+	Employee(String name, int id)
+	{
+		this.name = name;
+		this.id = id;
+	}
+
+	void getData()
+	{
+		System.out.println("Name is " + name);
+		System.out.println("Id is " + id);
+	}
+
+	public Object clone() throws CloneNotSupportedException
+	{
+		return super.clone();
+	}
+}
+
+class Methods
+{
+	public static void main(String args[]) throws CloneNotSupportedException
+	{
+		Employee obj1 = new Employee("sanchit", 19);
+		obj1.getData();
+		Employee obj2 = (Employee)obj1.clone();
+		obj2.getData();
+	}
+}
