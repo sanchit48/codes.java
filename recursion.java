27604b60242cf4958a5ba0be200b7f9e458801c3
Sanchit Khare <Sanchitkhare3@gmail.com>
2019-06-09 16:17:59 +0530

    Update recursion.java

--- recursion.java
+++ recursion.java
@@ -1,19 +1,15 @@
-import java.util.*;
 class QueueDemo         
 {
 	int n;
-	static void foo(int n)
-	{
+	static void foo(int n) {
 		if(n<1)
 			return;
 		else
 			foo(n-1);
 		System.out.println(n);
 	}
-	public static void main(String args[]) throws Exception
+	public static void main(String args[])
 	{
 		foo(3);
 	}
-		
-	
 }
